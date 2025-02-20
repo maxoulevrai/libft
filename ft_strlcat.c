cff99c929b7a3d947f7523d82d3050556c0051f8
maxoulevrai <maxime.leca976@gmail.com>
2025-02-20 18:16:44 +0100

    libft

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 18:58:13 by root              #+#    #+#             */
-/*   Updated: 2025/02/20 18:12:50 by root             ###   ########.fr       */
+/*   Updated: 2025/02/20 18:16:36 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,16 @@ size_t	ft_strlcat(char *dst, const char *src, size_t siz)
 	return (src_len + dst_len);
 }
 
-int	main(int ac, char **av)
-{
-	char	dst[64] = "alorsla";
-	char	dst2[64] = "alorsla";
-	size_t	siz = 1;
+// int	main(int ac, char **av)
+// {
+// 	char	dst[64] = "alorsla";
+// 	char	dst2[64] = "alorsla";
+// 	size_t	siz = 1;
 
-	(void)ac;
-	printf("%ld\n", ft_strlcat(dst, av[1], siz));
-	printf("%s\n", dst);
-	printf("%ld\n", strlcat(dst2, av[1], siz));
-	printf("%s\n", dst2);
-	return (0);
-}
+// 	(void)ac;
+// 	printf("%ld\n", ft_strlcat(dst, av[1], siz));
+// 	printf("%s\n", dst);
+// 	printf("%ld\n", strlcat(dst2, av[1], siz));
+// 	printf("%s\n", dst2);
+// 	return (0);
+// }
